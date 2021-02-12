8ce984bb00b63720458a8ec5c85fefcea04128b1
Nikita <knanaprimer@gmail.com>
2021-02-12 14:44:32 +0300

    upgrade "switch" аварийный переход и default

--- .vscode/switch.js
+++ .vscode/switch.js
@@ -2,13 +2,17 @@ switch(totalBet) {
     case 7:
         totalBet = funds;
         break;
-    case 11:
-        totalBet = 0;
-        break;
     case 13:
+        funds= funds - 1; //подать пенс на милыстыню.
+        //нет break, так что переходим на case 11 
+    case 11:
+
         totalBet =  0;
         break;
     case 21:
         totalBet = 21;
         break;
+    default:
+        console.log('здесь нет никаких суеверий!');
+        break; //break не обязателен, так как нет разделов дальше, но это хороший тон
 }
