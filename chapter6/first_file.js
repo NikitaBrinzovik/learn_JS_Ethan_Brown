4a820dd75aec4585426de78f002c555d0bb9831d
Nikita <knanaprimer@gmail.com>
2021-02-15 18:11:41 +0300

    Chapter 6 untill "this"

---
+++ chapter6/first_file.js
@@ -0,0 +1,71 @@
+
+function getGreeting() {
+    return 'Hello world!';
+}
+
+
+const arr =[1, 2, 3];
+arr[1] = getGreeting;
+arr[1] ();
+
+function getSentence({ subject, verb, object }) {
+    return `${subject} ${verb} ${object}`;
+}
+const o = {
+    subject: 'I',
+    verb: 'love',
+    object: 'JavaScript',
+}
+
+getSentence(o);
+
+//можно и деструктурировать массив
+
+function getSentence([ subject, verb, object ]) {
+    return `${subject} ${verb} ${object}`;
+}
+
+const arr = [ 'I', 'love', 'JavaScript' ];
+getSentence(arr);
+
+//оператор расширения(...) для сбора любых доп аргументов
+
+function addPrefix(prefix, ...words) {
+    //это не самый лучший способ
+    const prefixedWords = [];
+    for(let i=0; i<words.length; i++) {
+        prefixedWords[i] = prefix + words[i];
+    }
+    return prefixedWords;
+}
+addPrefix('con', 'verse', 'vex'); //'converse and convex
+
+
+function f(a, b = 'default', c = 3) {
+    return `${a} - ${b} - ${c}`;
+}
+f(5, 6, 7); //'5-6-7'
+f(5, 6) ; //'5-6-3'
+f(5) ; // ' 5-default-3'
+f(); //'undefined-default-3'
+
+
+const o2 = {
+    name: "wal", ////свойство базового типа
+    bark: function() { return 'Woof!'; } //свойство функции метод
+}
+// можно короче
+const o3 = {
+    name: 'Wal',
+    bark() {return 'Woof!';},
+}
+
+//КЛЮЧЕВОЕ THIS
+
+const o4 = {
+    name: 'Wal',
+    speak() {return `My name is ${this.name}!`; },
+}
+o4.speak()
+
+
