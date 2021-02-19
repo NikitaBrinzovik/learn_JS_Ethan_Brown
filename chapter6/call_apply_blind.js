c0c6a1d0178bf60e9d3d7fa59dcd0e597c4c4719
Nikita <knanaprimer@gmail.com>
2021-02-19 14:11:01 +0300

    BIND APPLY CALL, end of chapter 6

--- chapter6/call_apply_blind.js
+++ chapter6/call_apply_blind.js
@@ -6,8 +6,8 @@ function greet() {
     return `Привет! Меня зовут ${this.name}!`;
 }
 greet() // ПРивет! меня зовут ! - this не привязана
-getGreeting.call(bruce) ; // привет! меня зовут Bruce - 'this'  привяз к брюс
-getGreeting.call(madeline) ; // привет! меня зовут madeline - привяз madeline
+greet.call(bruce) ; // привет! меня зовут Bruce - 'this'  привяз к брюс
+greet.call(madeline) ; // привет! меня зовут madeline - привяз madeline
 
 
 function update(birthYear, occupation) {
@@ -16,3 +16,31 @@ function update(birthYear, occupation) {
 }
 update.call(bruce, 1949, 'singer');
 update.call(madeline, 1942, 'actress');
+
+//APPLY
+
+update.apply(bruce, [1955, 'actor']);
+//bruce теперь { name: 'Bruce', birthYear: 1955, occupation: 'actor'}
+update.apply(madeline, [1918, 'writer']);
+
+const arr = [2, 3, -5, 15, 7];
+Math.min.apply(null, arr); // тут вместо this стоит null так как в функцие
+Math.max.apply(null, arr); // Math.max and Math.min не используется this вообще
+
+const newBruce = [1940, 'martial artist'];
+update.call(bruce, ...newBruce); //эквивалент apply(bruce, newBruce)
+Math.min(...arr);
+Math.max(...arr);
+
+//BIND
+
+const updateBruce = update.bind(bruce);
+
+updateBruce(1904, 'actor');
+//bruce теперь name; bruce, birthYear: 1904, occupation: 'actor'
+updateBruce.call(madeline, 1274, 'king');
+//bruce теперь  name:Bruce, birthYear: 1274, occupation: 'king'
+//madeline не было присвоенно!!!!
+
+const updateBruce1949 =update.bind(bruce, 1949);
+updateBruce1949('singer, writer');
